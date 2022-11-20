2f03fc2a6823da3776352035131603a69d55cec1
JeongTaek Han <smart8612@gmail.com>
2022-11-20 12:54:12 +0900

    feat: SudokuGame.py 데모 코드 작성 #3

---
+++ Model/SudokuGame.py
@@ -0,0 +1,11 @@
+class SudokuGame:
+    def __init__(self):
+        print("Init sudoku game")
+
+    def run(self):
+        print("Run sudoku game")
+
+    def solve(self):
+        print("solve")
+
+SudokuGame()
