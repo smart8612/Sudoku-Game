508d5e3abea3ebd8ee887e1df70bb694466cebb0
JeongTaek Han <smart8612@gmail.com>
2022-11-20 14:32:43 +0900

    feat: 게임 알고리즘 구현용 메서드 정의 #3

--- Model/SudokuGame.py
+++ Model/SudokuGame.py
@@ -4,11 +4,13 @@ import copy
 class SudokuGame:
     pane = []
 
-    def __init__(self):
-        print("Init sudoku game")
+    MAX_SIZE = 4
+    UNASSIGNED = 0
 
-    def run(self):
-        print("Run sudoku game")
+    def __init__(self):
+        self.pane = [
+            [0]*self.MAX_SIZE for _ in range(self.MAX_SIZE)
+        ]
 
     def write(self, pane) -> None:
         self.pane = copy.deepcopy(pane)
@@ -16,3 +18,22 @@ class SudokuGame:
     def solve(self) -> bool:
         print("solve")
         return True
+
+    def __find_unassigned_position(self, row, col) -> bool:
+        pass
+
+    def __is_promising(self, row, col, number) -> bool:
+        pass
+
+    def __is_used_in_row(self, row, number) -> bool:
+        pass
+
+    def __is_used_in_col(self, col, number) -> bool:
+        pass
+
+    def __is_used_in_box(self, row, col, number) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    game = SudokuGame()
