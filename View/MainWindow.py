de2bf4a2f3dd5ad55e107d9e2174355c5befe38d
JeongTaek Han <smart8612@gmail.com>
2022-11-20 21:15:39 +0900

    feat: 윈도우 고정 크기 설정 #4

--- View/MainWindow.py
+++ View/MainWindow.py
@@ -1,8 +1,10 @@
 from PyQt6.QtWidgets import QMainWindow, QPushButton
+from PyQt6.QtCore import QSize
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(QSize(500, 500))
         self.button = QPushButton("Push for Window")
         self.setCentralWidget(self.button)
