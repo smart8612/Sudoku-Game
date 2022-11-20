e57aa5093a234bf94cfc29e78222ea4b3e1926b1
JeongTaek Han <smart8612@gmail.com>
2022-11-20 13:50:40 +0900

    feat: SudokuGame.py 판 입력 write 메서드 추가 #3

--- Model/SudokuGame.py
+++ Model/SudokuGame.py
@@ -1,9 +1,14 @@
 class SudokuGame:
+    pane = [[]]
+
     def __init__(self):
         print("Init sudoku game")
 
     def run(self):
         print("Run sudoku game")
+
+    def write(self, pane):
+        print("Write sudoku game pane")
 
     def solve(self):
         print("solve")
